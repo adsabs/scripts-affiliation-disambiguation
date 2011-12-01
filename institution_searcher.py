3d72506cc20aa09c25121ad16a4b4d5e617a821c
Benoit Thiell <bthiell@cfa.harvard.edu>
2011-12-01 10:04:24 -0500

    Searcher: Add function to get a single match.

--- institution_searcher.py
+++ institution_searcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import re
 import solr
 import sys
 
@@ -11,7 +12,7 @@ def search_institution(institution):
     """
     Searches an institution and returns the best match i.e. the best result.
     """
-    response = CONNECTION.query(institution, defType='lucene')
+    response = CONNECTION.query(_clean_affiliation(institution))
     if response.numFound > 0:
         minimum_score = response.results[0]['score'] * SCORE_PERCENTAGE
         for result in response.results:
@@ -23,5 +24,22 @@ def search_institution(institution):
     else:
         print 'No result found.'
 
-if __name__ == '__main__':    
+def get_match(institution):
+    try:
+        response = CONNECTION.query(_clean_affiliation(institution))
+    except:
+        print _clean_affiliation(institution)
+        raise
+
+    if response.numFound > 0:
+        return response.results[0]['display_name']
+    else:
+        return None
+
+def _clean_affiliation(aff):
+    aff = re.sub('[()[\]:\-/&]', ' ', aff)
+    aff = re.sub('\s\s+', ' ', aff)
+    return aff.strip()
+
+if __name__ == '__main__':
     search_institution(sys.argv[-1])
